7d17d914323d68832ce11a9c63ab3c173a88965b
RafaelSGil <rafaelgil131@gmail.com>
2024-03-04 18:35:43 +0000

    Single-threaded solution

--- prog2/ex2.c
+++ prog2/ex2.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void print_int_array(int *arr, int size);
+void bitonicMerge(int arr[], int low, int count, int dir);
+void bitonicSort(int arr[], int low, int count, int dir);
+void sort(int arr[], int n, int up);
 
 int main(int argc, char *argv[])
 {
@@ -22,30 +28,46 @@ int main(int argc, char *argv[])
     {
         file = fopen (argv[i], "rb");
 
-        if(file == NULL) {
+        if(file == NULL ) {
             printf("Not able to open the file.");
             continue;
             // return 1;
         }
 
-        int num;
-        int *dynamicIntArray = NULL;
-        int count = 0;
-        while (fread(&num, sizeof(int), 1, file) != 0){   // fread will return 1 when it is able to read a number and 0 when it is not able to read a number (end of file)
-            
-            // allocate memory for the array and add the number to the array
-            dynamicIntArray = (int *)realloc(dynamicIntArray, ++count * sizeof(int));
-            if (dynamicIntArray == NULL) {
-                printf("Memory allocation failed. Exiting program.\n");
-                return 1; // Indicates an error
+        int array_size;
+        int err = fread(&array_size, sizeof(array_size), 1, file);
+        if(err <= 0){
+            printf("Error while reading file");
+            return 1;
+        }
+
+        int* number_array;
+        number_array = malloc(array_size * sizeof(int));
+        if(number_array == NULL){
+            printf("Error while allocating memory");
+            exit(1);
+        }
+
+        if(fread(number_array, sizeof(int), array_size, file) != array_size){
+            // printf("%d ", num_read);
+            printf("Error while reading numbers to array");
+            exit(1);
+        }
+
+        //print_int_array(array, array_size);
+        sort(number_array ,array_size, 0);
+        //print_int_array(array, array_size);
+
+        for (int i = 1; i < array_size; i++){
+            if (number_array[i - 1] < number_array[i]){ 
+                printf ("Error in position %d between element %d and %d\n", i, number_array[i - 1], number_array[i]);
+                break;
             }
-            dynamicIntArray[count-1] = num;
+            if (i == (array_size - 1)){
+                printf ("Everything is OK!\n");
+            }
         }
-
-        // print int array
-        print_int_array(dynamicIntArray, count);
-
-        }
+    }
 
 
 
@@ -65,4 +87,38 @@ void print_int_array(int *arr, int size){
     printf("]\n");
 }
 
+void bitonicMerge(int arr[], int low, int count, int dir) {
+    if (count > 1) {
+        int k = count / 2;
+        for (int i = low; i < low + k; i++) {
+            if ((arr[i] > arr[i + k]) == dir) {
+                // Swap elements if they are in the wrong order
+                int temp = arr[i];
+                arr[i] = arr[i + k];
+                arr[i + k] = temp;
+            }
+        }
+        bitonicMerge(arr, low, k, dir);
+        bitonicMerge(arr, low + k, k, dir);
+    }
+}
 
+// Function to perform bitonic sort recursively
+void bitonicSort(int arr[], int low, int count, int dir) {
+    if (count > 1) {
+        int k = count / 2;
+        
+        // Sort in ascending order
+        bitonicSort(arr, low, k, 0);
+        
+        // Sort in descending order
+        bitonicSort(arr, low + k, k, 1);
+        
+        // Merge both sorted halves
+        bitonicMerge(arr, low, count, dir);
+    }
+}
+
+void sort(int arr[], int n, int up) {
+    bitonicSort(arr, 0, n, up);
+}
--- prog2/ex2.sh
+++ prog2/ex2.sh
@@ -4,11 +4,11 @@
 # gcc -Wall -O3 -o countWords countWords.c
 # -Wall: All warnings should be printed
 # -O3: Level 3 of optimization (Maximum level of optimization)
-compile_output=$(gcc prog2/ex2.c -Wall -O3 -o prog2/ex2)
+compile_output=$(gcc ex2.c -Wall -O3 -o ex2)
 
 if [ $? -eq 0 ]; then
     echo "Compilation successful"
-    ./prog2/ex2 "$@"
+    ./ex2 "$@"
 else
     echo "Compilation failed"
 fi
