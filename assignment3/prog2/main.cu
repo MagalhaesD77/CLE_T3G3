e9770e3c758e78283dff53b28f5838afb40a085b
RafaelSGil <rafaelgil131@gmail.com>
2024-05-29 22:04:35 +0100

    Implemented CUDA bitonic sort - row version

--- assignment3/prog2/main.cu
+++ assignment3/prog2/main.cu
@@ -2,9 +2,9 @@
  *  \file main.c (implementation file)
  *
  *  \brief Problem name: Bitonic sort.
- *  
+ *
  *  CUDA
- * 
+ *
  *  \author Rafael Gil & Diogo Magalhães - May 2024
  */
 
@@ -16,24 +16,24 @@
 #include "common.h"
 #include <cuda_runtime.h>
 
-#define N 1024  // length of the row 
+#define N 1024 // length of the row
 
 /**
  * \brief Read file and populate data array
- * 
+ *
  * \param fileName name of the file to read
  * \param lenNumberArray size of the array
  * \param numberArray array to be checked
-*/
+ */
 void readFile(char *fileName, int *lenNumberArray, int **numberArray);
 
 /**
  * \brief checks if the number array is ordered in decreasing order
- * 
+ *
  * \param lenNumberArray size of the array
  * \param numberArray array to be checked
-*/
-void verifyIfSequenceIsOrdered(int lenNumberArray, int **numberArray);
+ */
+void verifyIfSequenceIsOrdered(int *lenNumberArray, int **numberArray);
 
 /**
  *  \brief implementation of the imperitive bitonic sort - descending order.
@@ -63,11 +63,13 @@ static double get_delta_time(void);
  *
  *  \return status of operation
  */
-int main(){
+int main(int argc, char **argv)
+{
 
     printf("%s Starting...\n", argv[0]);
 
-    if (argc != 2){
+    if (argc != 2)
+    {
         fprintf(stderr, "Invalid number of arguments\n");
         exit(EXIT_FAILURE);
     }
@@ -76,63 +78,66 @@ int main(){
     int dev = 0;
 
     cudaDeviceProp deviceProp;
-    CHECK (cudaGetDeviceProperties (&deviceProp, dev));
+    CHECK(cudaGetDeviceProperties(&deviceProp, dev));
     printf("Using Device %d: %s\n", dev, deviceProp.name);
-    CHECK (cudaSetDevice (dev)); // the gpu its going to use
+    CHECK(cudaSetDevice(dev)); // the gpu its going to use
 
     // number sequence
     int *numberSequence = NULL;
-    
+
     // length of number sequence
     int lenNumberSequence = 0;
 
     // read sequence from file
     readFile(argv[1], &lenNumberSequence, &numberSequence);
 
-    if(lenNumberSequence > (size_t)5e9){
+    if (lenNumberSequence > (size_t)5e9)
+    {
         fprintf(stderr, "Sequence too big. The GPU can only withstand 5Gb of data\n");
         exit(EXIT_FAILURE);
     }
 
     // alocate memory for the sequence in the GPU
     int *gpuSequence = NULL;
-    CHECK(cudaMalloc((void**)&gpuSequence, lenNumberSequence * sizeof(int)));
+    CHECK(cudaMalloc((void **)&gpuSequence, lenNumberSequence * sizeof(int)));
 
     // copy sequence from host to gpu
     CHECK(cudaMemcpy(gpuSequence, numberSequence, lenNumberSequence * sizeof(int), cudaMemcpyHostToDevice));
 
     // kernel configuration
     unsigned int gridDimX, gridDimY, gridDimZ, blockDimX, blockDimY, blockDimZ;
-	
-	blockDimX = 1 << 0; // optimize!
-	blockDimY = 1 << 0; // optimize!
-	blockDimZ = 1 << 0; // do not change!
-	gridDimX = 1 << 0; 	// optimize!
-	gridDimY = 1 << 0;  // optimize!
-	gridDimZ = 1 << 0;  // do not change!
+
+    blockDimX = 1 << 0;
+    blockDimY = 1 << 0;
+    blockDimZ = 1 << 0;
+    // gridDimX = 1 << 1;
+    gridDimY = 1 << 0;
+    gridDimZ = 1 << 0;
 
     // define the number of iterations necessary
-    int nIter = (int) (log2 (N) + 1.1);
+    int nIter = (int)(log2(N) + 1.1);
 
-    (void) get_delta_time();
+    (void)get_delta_time();
 
     for (int i = 0; i < nIter; i++)
     {
         // DO THE ITERATION AND SORTING PROCESS
 
+        gridDimX = pow(2, (nIter - i) - 1);
+
         dim3 grid(gridDimX, gridDimY, gridDimZ);
-		dim3 block(blockDimX, blockDimY, blockDimZ);
+        dim3 block(blockDimX, blockDimY, blockDimZ);
 
         // calculate the size of the sub-sequence
         int lenSubSeqeunce = (int)(1 << i) * N;
-
+        printf("Current iteration: %d / Length sub-sequence: %d / Grid dimension: %d\n", i, lenSubSeqeunce, (int)pow(2, (nIter - i) - 1));
         bitonicSortOnGPU<<<grid, block>>>(gpuSequence, lenSubSeqeunce, i);
 
         // synchronize and wait for all the threads
         CHECK(cudaDeviceSynchronize());
 
-        // check if any error occured	
-		CHECK(cudaGetLastError());
+        // check if any error occured
+        CHECK(cudaGetLastError());
     }
 
     printf("\nElapsed time = %.6f s\n", get_delta_time());
@@ -141,40 +146,45 @@ int main(){
     CHECK(cudaMemcpy(numberSequence, gpuSequence, lenNumberSequence * sizeof(int), cudaMemcpyDeviceToHost));
 
     // free alocated memory on the gpu
-    CHECK(cudeFree(gpuSequence))
+    CHECK(cudaFree(gpuSequence));
 
     // reset the gpu
-    CHECK(cudaDeviceReset())
+    CHECK(cudaDeviceReset());
 
-    //check if array is correctly ordered
-    verifyIfSequenceIsOrdered(lenNumberSequence, &numberSequence);
+    // check if array is correctly ordered
+    verifyIfSequenceIsOrdered(&lenNumberSequence, &numberSequence);
 
     // free alocated memory
-    free(numberSequence)
+    free(numberSequence);
 
     exit(EXIT_SUCCESS);
 }
 
-void readFile(char *fileName, int *lenNumberArray, int **numberArray) {
+void readFile(char *fileName, int *lenNumberArray, int **numberArray)
+{
     FILE *file = fopen(fileName, "rb");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         fprintf(stderr, "Unable to open the file.\n");
         exit(EXIT_FAILURE);
     }
 
     int err = fread(lenNumberArray, sizeof(int), 1, file);
-    if (err <= 0) {
+    if (err <= 0)
+    {
         fprintf(stderr, "Error while reading file.\n");
         exit(EXIT_FAILURE);
     }
 
-    *numberArray = malloc(*lenNumberArray * sizeof(int));
-    if (*numberArray == NULL) {
+    *numberArray = (int *)malloc(*lenNumberArray * sizeof(int));
+    if (*numberArray == NULL)
+    {
         fprintf(stderr, "Memory allocation failed.\n");
         exit(EXIT_FAILURE);
     }
 
-    if (fread(*numberArray, sizeof(int), *lenNumberArray, file) != *lenNumberArray) {
+    if (fread(*numberArray, sizeof(int), *lenNumberArray, file) != *lenNumberArray)
+    {
         fprintf(stderr, "Error while reading numbers to array.\n");
         exit(EXIT_FAILURE);
     }
@@ -182,9 +192,12 @@ void readFile(char *fileName, int *lenNumberArray, int **numberArray) {
     fclose(file);
 }
 
-void verifyIfSequenceIsOrdered(int lenNumberArray, int **numberArray) {
-    for (int i = 0; i < lenNumberArray - 1; i++) {
-        if ((*numberArray)[i] < (*numberArray)[i + 1]) {
+void verifyIfSequenceIsOrdered(int *lenNumberArray, int **numberArray)
+{
+    for (int i = 0; i < *lenNumberArray - 1; i++)
+    {
+        if ((*numberArray)[i] < (*numberArray)[i + 1])
+        {
             printf("Error in position %d between elements %d and %d\n", i, (*numberArray)[i], (*numberArray)[i + 1]);
             return;
         }
@@ -193,39 +206,11 @@ void verifyIfSequenceIsOrdered(int lenNumberArray, int **numberArray) {
     printf("Everything is OK!\n");
 }
 
-void imperativeBitonicSort(int *array, int N, int startIndex, int endIndex){
-    // iterate through the powers of 2 up to N
-    // simulates the layers of the algorithm
-    for (int k = 2; k <= N; k = 2 * k) {
-        // iterate through half of the current value of k
-        // controls the length of the comparison between the numbers
-        for (int j = k / 2; j > 0; j = j / 2) {
-            // iterates through the partition of the array
-            for (int i = startIndex; i <= endIndex; i++) {
-                int ij = i ^ j;     // bitwise XOR, to calculate the index where to perform the comparison
-                if ((ij) > i) {     // assure correct order
-                    if (((i & k) == 0                               // bitwise AND to check if i-th index is in the lower half of the bitonic sequence
-                                && array[i] < array[ij])            // check if i-th element is smaller than ij
-                        || ((i & k) != 0                            // bitwise AND to check if i-th index is in the upper half of the bitonic sequence
-                                && array[i] > array[ij])) {         // check if i-th element is greater than ij
-
-                        // performs a common swap between the elements of the array
-                        int aux = array[i];
-                        array[i] = array[ij];
-                        array[ij] = aux;
-                    }
-                }
-            }
-        }
-    }
-}
-
-
-__global__ void bitonicSortOnGPU(int *array, int lenSubSequence, int iter)
+__global__ void bitonicSortOnGPU(int *numArray, int lenSubSequence, int iter)
 {
     int x, y, idx;
 
-    // calculate the segment of the sequence for the current thread 
+    // calculate the segment of the sequence for the current thread
     x = (int)threadIdx.x + (int)blockDim.x * (int)blockIdx.x;
     y = (int)threadIdx.y + (int)blockDim.y * (int)blockIdx.y;
     idx = (int)blockDim.x * (int)gridDim.x * y + x;
@@ -233,52 +218,51 @@ __global__ void bitonicSortOnGPU(int *array, int lenSubSequence, int iter)
     if (lenSubSequence == N){   // bitonic sort
         // iterate through the powers of 2 up to N
         // simulates the layers of the algorithm
-        for (int k = 2; k <= lenSubSequence; k = 2 * k) {
+        for (int k = 2; k <= lenSubSequence; k *= 2){
             // iterate through half of the current value of k
             // controls the length of the comparison between the numbers
-            for (int j = k / 2; j > 0; j = j / 2) {
+            for (int j = k / 2; j > 0; j /= 2){
                 // iterates through the partition of the array
-                for (int i = 0; i <= endIndex; i++) {
+                for (int i = 0; i < (1 << iter) * N; i++){
                     int elem = N * (1 << iter) * idx + i;
-                    int ij = elem ^ j;     // bitwise XOR, to calculate the index where to perform the comparison
-                    if ((ij) > elem) {     // assure correct order
-                        if (((elem & k) == 0                               // bitwise AND to check if i-th index is in the lower half of the bitonic sequence
-                                    && array[elem] < array[ij])            // check if i-th element is smaller than ij
-                            || ((elem & k) != 0                            // bitwise AND to check if i-th index is in the upper half of the bitonic sequence
-                                    && array[elem] > array[ij])) {         // check if i-th element is greater than ij
-
+                    int l = elem ^ j;           // bitwise XOR, to calculate the index where to perform the comparison
+                    if (l > elem){              // assure correct order
+                        if ((((elem & k) == 0)                          // bitwise AND to check if i-th index is in the lower half of the bitonic sequence
+                                && (numArray[elem] < numArray[l]))      // check if i-th element is smaller than ij
+                                || (((elem & k) != 0)                   // bitwise AND to check if i-th index is in the upper half of the bitonic sequence
+                                && (numArray[elem] > numArray[l]))){     // check if i-th element is greater than ij
+                        
                             // performs a common swap between the elements of the array
-                            int aux = array[elem];
-                            array[elem] = array[ij];
-                            array[ij] = aux;
+                            int aux = numArray[elem];
+                            numArray[elem] = numArray[l];
+                            numArray[l] = aux;
                         }
                     }
                 }
             }
         }
-    }else{  // bitonic merge
+    }else{      // bitonic merge
         int k = lenSubSequence;
-        for (int j = k / 2; j > 0; j = j / 2) {
+        for (int j = k / 2; j > 0; j /= 2){
                 // iterates through the partition of the array
-                for (int i = 0; i <= endIndex; i++) {
+                for (int i = 0; i < (1 << iter) * N; i++){
                     int elem = N * (1 << iter) * idx + i;
-                    int ij = elem ^ j;     // bitwise XOR, to calculate the index where to perform the comparison
-                    if ((ij) > elem) {     // assure correct order
-                        if (((elem & k) == 0                               // bitwise AND to check if i-th index is in the lower half of the bitonic sequence
-                                    && array[elem] < array[ij])            // check if i-th element is smaller than ij
-                            || ((elem & k) != 0                            // bitwise AND to check if i-th index is in the upper half of the bitonic sequence
-                                    && array[elem] > array[ij])) {         // check if i-th element is greater than ij
-
+                    int l = elem ^ j;           // bitwise XOR, to calculate the index where to perform the comparison
+                    if (l > elem){              // assure correct order
+                        if ((((elem & k) == 0)                          // bitwise AND to check if i-th index is in the lower half of the bitonic sequence
+                                && (numArray[elem] < numArray[l]))      // check if i-th element is smaller than ij
+                                || (((elem & k) != 0)                   // bitwise AND to check if i-th index is in the upper half of the bitonic sequence
+                                && (numArray[elem] > numArray[l]))){     // check if i-th element is greater than ij
+                        
                             // performs a common swap between the elements of the array
-                            int aux = array[elem];
-                            array[elem] = array[ij];
-                            array[ij] = aux;
+                            int aux = numArray[elem];
+                            numArray[elem] = numArray[l];
+                            numArray[l] = aux;
                         }
                     }
                 }
-        }
+            }
     }
-    
 }
 
 static double get_delta_time(void)
